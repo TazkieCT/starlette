22f2c63fbf294f112467fb4ef4cb7002ed0f65cb
Ahingg <Xaviero.yamin28@gmail.com>
2025-05-20 03:10:11 +0700

    feat: Added second room's part 2 puzzle teleport

---
+++ Starlette/Assets/Scripts/Puzzles/SecondRoom/SecondPart.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SecondPart : MonoBehaviour, Interactable
+{
+    public void Interact()
+    {
+        // Implement the interaction logic here
+        Debug.Log("Interacting with SecondPart");
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
